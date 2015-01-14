530564bdc462b3b0739a077e254614d1b74f6093
Neal Grantham <ngranth@ncsu.edu>
2015-01-14 16:26:39 -0500

    concisely loads all db tables into R

--- load-database.R
+++ load-database.R
@@ -4,26 +4,20 @@ rm(list = ls())  # fresh workspace
 library(RSQLite)
 library(DBI)
 
+# Connect to database
 con <- dbConnect(RSQLite::SQLite(), "pitchfork-reviews.db")
-dbListTables(con)
 
-# You can fetch all results:
-results <- dbSendQuery(con, "SELECT * FROM albums")
-albums <- dbFetch(results)
-dbClearResult(results)
-
-# You can fetch all results:
-results <- dbSendQuery(con, "SELECT * FROM artists")
-artists <- dbFetch(results)
-dbClearResult(results)
-
-# You can fetch all results:
-results <- dbSendQuery(con, "SELECT * FROM reviewers")
-reviewers <- dbFetch(results)
-dbClearResult(results)
+# Create R Dataframes from SQL Tables
+fetch_and_store_results <- function(tab, env) {
+  query <- paste("SELECT * FROM", tab)
+  results <- dbSendQuery(con, query)
+  assign(tab, dbFetch(results), envir = env)
+  dbClearResult(results)
+}
+tables <- dbListTables(con)
+sapply(tables, fetch_and_store_results, env = environment())
 
 # Disconnect from the database
 dbDisconnect(con)
-
-rm(list = c("con", "results"))
+rm(list = c("con", "tables", "fetch_and_store_results"))
 
